eac77343d3e2f19a8203a8b1aab02545310086de
y-minion <dudals2963@tukorea.ac.kr>
2025-11-03 23:05:59 +0900

    useSignUp훅 에러 핸들링

--- src/hooks/mutations/use-sign-up.ts
+++ src/hooks/mutations/use-sign-up.ts
@@ -4,5 +4,8 @@ import { useMutation } from "@tanstack/react-query";
 export function useSignUp() {
   return useMutation({
     mutationFn: signUp,
+    onError: (error) => {
+      alert(`에러 발생!: ${error}`);
+    },
   });
 }
