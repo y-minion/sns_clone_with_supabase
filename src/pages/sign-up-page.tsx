8515ea52a10606b2cd433f07d7a33379d1872b51
y-minion <dudals2963@tukorea.ac.kr>
2025-11-03 18:39:23 +0900

    회원가입 페이지 레이아웃 구현

--- src/pages/sign-up-page.tsx
+++ src/pages/sign-up-page.tsx
@@ -1,3 +1,46 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { Link } from "react-router";
+
 export default function SignUpPage() {
-  return <div>SignUpPage</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSignUpClick = () => {
+    if (email.trim() === "") return;
+    if (password.trim() === "") return;
+
+    //유효성 검사가 통과 되면 supabase에 회원가입 요청을 보내야한다.
+  };
+
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="text-xl font-bold">회원가입</div>
+      <div className="flex flex-col gap-2">
+        <Input
+          className="py-6"
+          type="email"
+          placeholder="이메일"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          className="py-6"
+          type="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <div>
+        <Button className="w-full">회원가입</Button>
+      </div>
+      <div>
+        <Link className="text-muted-foreground hover:underline" to={"/sign-in"}>
+          이미 계정이 있다면? 로그인
+        </Link>
+      </div>
+    </div>
+  );
 }
