4bf9e3c148aca92688b0d300f2c542692ba0a818
y-minion <dudals2963@tukorea.ac.kr>
2025-11-03 18:40:06 +0900

    supabase클라이언트를 이용해 auth의 signup메서드에 사용자가 입력한 email과 password를 통해 회원가입을 진행하는 api구현완료

---
+++ src/api/auth.ts
@@ -0,0 +1,14 @@
+import supabase from "@/lib/supabase";
+
+export async function signUp({
+  email,
+  password,
+}: {
+  email: string;
+  password: string;
+}) {
+  const { data, error } = await supabase.auth.signUp({ email, password });
+
+  if (error) throw error;
+  return data;
+}
