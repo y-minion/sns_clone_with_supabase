33277ca0226fd19c1379b3afea9da438fb684634
y-minion <dudals2963@tukorea.ac.kr>
2025-11-04 20:33:37 +0900

    비즈니스 로직 담당인 useSignWithPassword 훅이 에러 발생시 전달받은 callbacks.onError콜백함수를 실행한다.

--- src/hooks/mutations/use-sign-in-with-password.ts
+++ src/hooks/mutations/use-sign-in-with-password.ts
@@ -1,8 +1,12 @@
 import { signInWithPassword } from "@/api/auth";
+import type { UseMutationCallback } from "@/types";
 import { useMutation } from "@tanstack/react-query";
 
-export function useSignWithPassword() {
+export function useSignWithPassword(callbacks?: UseMutationCallback) {
   return useMutation({
     mutationFn: signInWithPassword,
+    onError: (error) => {
+      if (callbacks?.onError) callbacks.onError(error);
+    },
   });
 }
