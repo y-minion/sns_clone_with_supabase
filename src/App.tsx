63afd0abfcc7960cedfac0a6fb92bb785712790e
y-minion <dudals2963@tukorea.ac.kr>
2025-11-05 15:29:45 +0900

    유저의 auth이벤트 발생시 최상위 app.tsx에서 이벤트 감지해 세션 스토어에 세션 업데이트하는 로직 구현.
    supabase클라이언트의 supabase.auth.onAuthStateChange 메서드 사용.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import { useEffect } from "react";
 import RootRoute from "./root-route";
+import supabase from "./lib/supabase";
+import { useSetSession } from "./store/session";
 
 export default function App() {
+  const setSession = useSetSession();
+  useEffect(() => {
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === "SIGNED_OUT") {
+        setSession(null);
+      } else if (session) {
+        setSession(session);
+      }
+    });
+
+    return () => data.subscription.unsubscribe();
+  }, []);
+
   return <RootRoute />;
 }
