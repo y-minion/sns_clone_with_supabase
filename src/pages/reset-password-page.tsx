46a7865cc6b263f7762a7223fcba8af5ad5b39d8
y-minion <dudals2963@tukorea.ac.kr>
2025-11-05 21:16:21 +0900

    비밀번호 초기화 ui컴포넌트 레이아웃 구현

--- src/pages/reset-password-page.tsx
+++ src/pages/reset-password-page.tsx
@@ -1,3 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function ResetPasswordPage() {
-  return <div>ResetPasswordPage</div>;
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-1">
+        <div className="text-2xl font-bold">비밀번호 재설정하기</div>
+        <div className="text-muted-foreground">
+          새로운 비밀번호를 입력하세요.
+        </div>
+      </div>
+
+      <Input className="py-6" type="password" placeholder="password" />
+      <Button className="w-full" onClick={() => {}}>
+        비밀번호 변경하기
+      </Button>
+    </div>
+  );
 }
