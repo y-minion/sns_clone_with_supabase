936481d7f33f5c148eeae60953afd98cf9efa92b
y-minion <dudals2963@tukorea.ac.kr>
2025-11-05 15:59:19 +0900

    기존의 auth리스너 컴포넌트를 프로바이더로 분리완료.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,10 @@
-import { useEffect } from "react";
 import RootRoute from "./root-route";
-import supabase from "./lib/supabase";
-import { useSetSession } from "./store/session";
+import { SessionProvider } from "./provider/session-provider";
 
 export default function App() {
-  const setSession = useSetSession();
-  useEffect(() => {
-    const { data } = supabase.auth.onAuthStateChange((event, session) => {
-      if (event === "SIGNED_OUT") {
-        setSession(null);
-      } else if (session) {
-        setSession(session);
-      }
-    });
-
-    return () => data.subscription.unsubscribe();
-  }, []);
-
-  return <RootRoute />;
+  return (
+    <SessionProvider>
+      <RootRoute />
+    </SessionProvider>
+  );
 }
---
+++ src/provider/session-provider.tsx
@@ -0,0 +1,25 @@
+import GlobalLoader from "@/components/global-loader";
+import supabase from "@/lib/supabase";
+import { useIsSessionLoaded, useSetSession } from "@/store/session";
+import { useEffect, type ReactNode } from "react";
+
+export function SessionProvider({ children }: { children: ReactNode }) {
+  const setSession = useSetSession();
+  const isSessionLoaded = useIsSessionLoaded();
+
+  useEffect(() => {
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      if (event === "SIGNED_OUT") {
+        setSession(null);
+      } else if (session) {
+        setSession(session);
+      }
+    });
+
+    return () => data.subscription.unsubscribe();
+  }, []);
+
+  if (!isSessionLoaded) return <GlobalLoader />;
+
+  return children;
+}
