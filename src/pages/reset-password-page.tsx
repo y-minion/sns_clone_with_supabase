a1d071486000ee865930a752983ff1dc76108b3b
y-minion <dudals2963@tukorea.ac.kr>
2025-11-06 16:26:25 +0900

    비밀번호 변경 페이지 컴포넌트 구현 완료.
    콜백함수로 성공,실패시의 렌더링 로직은 클라이언트 컴포넌트에서 담당해 관심사 분리 완료.

--- src/pages/reset-password-page.tsx
+++ src/pages/reset-password-page.tsx
@@ -1,7 +1,37 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import useUpdateNewPassword from "@/hooks/mutations/use-reset-password";
+import { generateErrorMessage } from "@/lib/error";
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { toast } from "sonner";
 
 export default function ResetPasswordPage() {
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleUpdatePasswordClick = () => {
+    if (password.trim() === "") return;
+    updateNewPassword(password);
+  };
+
+  const { mutate: updateNewPassword, isPending: isUpdateNewPasswordPending } =
+    useUpdateNewPassword({
+      onError(error) {
+        const message = generateErrorMessage(error);
+        toast.error(message, {
+          position: "top-center",
+        });
+        setPassword("");
+      },
+      onSuccess() {
+        toast.success("비밀번호가 성공적으로 변경되었습니다.", {
+          position: "top-center",
+        });
+        navigate("/");
+      },
+    });
+
   return (
     <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-1">
@@ -11,8 +41,18 @@ export default function ResetPasswordPage() {
         </div>
       </div>
 
-      <Input className="py-6" type="password" placeholder="password" />
-      <Button className="w-full" onClick={() => {}}>
+      <Input
+        disabled={isUpdateNewPasswordPending}
+        onChange={(e) => setPassword(e.target.value)}
+        className="py-6"
+        type="password"
+        placeholder="password"
+      />
+      <Button
+        disabled={isUpdateNewPasswordPending}
+        className="w-full"
+        onClick={handleUpdatePasswordClick}
+      >
         비밀번호 변경하기
       </Button>
     </div>
